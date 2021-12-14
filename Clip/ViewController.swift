d840c8e16cd9b61bf948f7304007edbc4483fef6
nkalupahana <avishkank@gmail.com>
2021-12-14 15:08:21 -0600

    Initial Commit

---
+++ Clip/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Clip
+//
+//  Created by Nisala on 12/14/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
